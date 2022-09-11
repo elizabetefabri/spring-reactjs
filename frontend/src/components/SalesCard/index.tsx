4a1966b2d32959daa3b0473892466e0e8dc1976e
Elizabete Fabri <elizabete958@gmail.com>
2022-09-11 15:22:14 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, {
+  getDefaultLocale,
+  setDefaultLocale,
+} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Sale } from "../../models/sale";
 import { BASE_URL } from "../../utils/request";
 
 import { NotificationButton } from "../NotificationButton";
-
 import "./styles.css";
 
 export function SalesCard() {
@@ -19,10 +21,15 @@ export function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
@@ -63,14 +70,14 @@ export function SalesCard() {
             {sales.map((sale) => {
               return (
                 <tr key={sale.id}>
-                  <td className="show992">#341</td>
-                  <td className="show576">{sale.date}</td>
+                  <td className="show992">#{sale.id}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
                   <td>
-                    {/* FORMATANDO OS VALORES COM CASAS DECIMAIS */}
-                    R${" "}
                     {sale.amount.toLocaleString("pt-br", {
                       style: "currency",
                       currency: "BRL",
--- frontend/src/models/sale.ts
+++ frontend/src/models/sale.ts
@@ -2,7 +2,6 @@ export type Sale = {
   id: number;
   sellerName: string;
   date: string;
-
   visited: number;
   deals: number;
   amount: number;
